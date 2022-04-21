ef402276dbdd8664ad44a0f4a58e56609f645f96
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2022-04-20 23:16:58 -0300

    django secret remove

--- corretorIA/settings.py
+++ corretorIA/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+k!r-7&zh+7r$0z+y4^wc5+)8&!(r-bik*1ycv**0w&^4=3zf_'
+SECRET_KEY = config('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
