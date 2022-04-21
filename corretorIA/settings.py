5884fbb030bbfa285244e11722e7fff0f0083c81
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2022-04-21 00:24:10 -0300

    allow hosts

--- corretorIA/settings.py
+++ corretorIA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = '/login'
 
